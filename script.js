01af86378f5ec42cf2bead5f29211f6db0b61bcd
4rthur12 <arthur.fantinelli@escola.pr.gov.br>
2025-09-15 08:00:02 -0300

    15/09 3

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ function mostraPalavrasChave () {
     const palavrasChave = processaTexto(texto);
 
 campoResultado.textContent = palavrasChave.join(",");
+}
 
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras [i] =palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencia(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1,p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    
 }
